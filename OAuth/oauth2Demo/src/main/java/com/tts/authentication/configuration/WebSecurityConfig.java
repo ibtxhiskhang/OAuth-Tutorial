e76a281abca9c6553b9b7903ab80182263fc8548
Ibtxhis Khang <79154883+ibtxhiskhang@users.noreply.github.com>
2021-05-18 16:40:39 -0400

    Add files via upload

---
+++ OAuth/oauth2Demo/src/main/java/com/tts/authentication/configuration/WebSecurityConfig.java
@@ -0,0 +1,59 @@
+package com.tts.authentication.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/home").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    @Bean
+    @Override
+    //// below we are essentially building the details or a user
+    //// this method call may say its deprecated but that's a warning
+    //// to advise you to not have this code in  production
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("password")
+                        .roles("USER")
+                        .build();
+
+        UserDetails secondUser =
+                User.withDefaultPasswordEncoder()
+                        .username("user2")
+                        .password("password")
+                        .roles("USER")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user, secondUser);
+
+
+//        return new InMemoryUserDetailsManager(user);
+    }
+}
+
+
---
+++ OAuth/oauth2Demo/src/test/java/com/tts/oauth2Demo/Oauth2DemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.tts.oauth2Demo;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
